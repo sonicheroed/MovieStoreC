4b09c93b22d9140ef10cea8e4920b71b5122db8f
Boris Serafimov <b.b.serafimov@gmail.com>
2024-11-10 12:08:14 +0200

    Movie repository

---
+++ MovieStoreC/MovieStoreC.DL/Repositories/MovieStaticDataRepository.cs
@@ -0,0 +1,22 @@
+﻿using MovieStoreC.DL.Interfaces;
+using MovieStoreC.DL.StaticData;
+using MovieStoreC.Models.DTO;
+
+namespace MovieStoreC.DL.Repositories
+{
+    internal class MovieStaticDataRepository : IMovieRepository
+    {
+        public List<Movie> GetAll()
+        {
+            return StaticDb.Movies;
+        }
+    }
+
+    internal class MovieMongoRepository : IMovieRepository
+    {
+        public List<Movie> GetAll()
+        {
+            return StaticDb.Movies;
+        }
+    }
+}
